96d68198cc8140f5915ed4497bd627e32e3b5d45
San12we <xangida@gmail.com>
2024-12-10 17:30:03 +0300

    made some changes

--- app/doctor/schedule.tsx
+++ app/doctor/schedule.tsx
@@ -2,11 +2,32 @@ import React from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { Agenda } from 'react-native-calendars';
 import { Card, Avatar } from 'react-native-paper';
+import PagerView from 'react-native-pager-view';
+import { eachWeekOfInterval, subDays, addDays, eachDayOfInterval, format } from 'date-fns';
 
 const timeToString = (time) => {
   const date = new Date(time);
   return date.toISOString().split('T')[0];
 };
+
+const dates = eachWeekOfInterval(
+  {
+    start: subDays(new Date(), 14),
+    end: addDays(new Date(), 14),
+  },
+  {
+    weekStartsOn: 1,
+  }
+).reduce((acc: Date[][], cur) => {
+  const allDays = eachDayOfInterval({
+    start: cur,
+    end: addDays(cur, 6),
+  });
+
+  acc.push(allDays);
+
+  return acc;
+}, []);
 
 const Schedule: React.FC = () => {
   const [items, setItems] = React.useState({});
@@ -52,6 +73,23 @@ const Schedule: React.FC = () => {
 
   return (
     <View style={styles.container}>
+      <PagerView style={styles.pagerView}>
+        {dates.map((week, i) => (
+          <View key={i}>
+            <View style={styles.row}>
+              {week.map((day, j) => {
+                const txt = format(day, 'EEEEE');
+                return (
+                  <View key={j} style={styles.cell}>
+                    <Text>{txt}</Text>
+                    <Text>{day.getDate()}</Text>
+                  </View>
+                );
+              })}
+            </View>
+          </View>
+        ))}
+      </PagerView>
       <Agenda
         items={items}
         loadItemsForMonth={loadItems}
@@ -68,6 +106,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  pagerView: {
+    flex: 1,
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  cell: {
+    width: 50,
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   item: {
     backgroundColor: 'white',
     marginVertical: 8,
@@ -80,5 +131,4 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: 30,
   },
-
 });
